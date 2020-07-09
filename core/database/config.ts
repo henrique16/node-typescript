db3590ba90aec6a37c36152ef9b7a9df757e161f
Gabriel Henrique <gabriel.holiveira@live.com>
2020-07-09 13:26:18 -0300

    ignore

---
+++ core/database/config.ts
@@ -0,0 +1,14 @@
+export = {
+    user: "Donald",
+    password: "Febra7412",
+    server: "brunchdbi.database.windows.net",
+    database: "brunchdb",
+    options: {
+        "encrypt": true
+    },
+    pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
+    }
+}
